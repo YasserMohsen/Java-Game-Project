013491ebece3c3efc89dbfcb9a0d82f709707deb
walid ahmed <walid_ahmed_elsayed@yahoo.com>
2017-02-04 18:29:25 +0200

    Adding The Computer Class

--- ServerTicTacServer/src/control/DBConnection.java
+++ ServerTicTacServer/src/control/DBConnection.java
@@ -19,7 +19,7 @@ public class DBConnection {
         try {
             DriverManager.registerDriver(new Driver());
 
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/TicTac","kazafy", "");
+             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/TicTac","walid", "");
 
             
         } catch (SQLException ex) {
