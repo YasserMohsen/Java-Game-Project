dd87c4049e03fd55ebca2629970cc62ddcfeef9b
terminator <muhammad.habib.dev@gmail.com>
2017-02-07 01:01:28 +0200

    merg

--- ClientTicTacToe/src/clienttictactoe/Client.java
+++ ClientTicTacToe/src/clienttictactoe/Client.java
@@ -4,11 +4,6 @@
  * and open the template in the editor.
  */
 package clienttictactoe;
-
-import com.restfb.DefaultFacebookClient;
-import com.restfb.FacebookClient;
-import com.restfb.Parameter;
-import com.restfb.types.FacebookType;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -24,8 +19,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import model.Request;
 import model.User;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 /**
  *
